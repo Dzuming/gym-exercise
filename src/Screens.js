7f01f250472b4eb9be3a449cd755990be99ea1bc
Dawid Poliszak <d.poliszak@binarapps.com>
2019-10-20 12:25:06 +0200

    Remove redundant navigation code

--- src/Screens.js
+++ src/Screens.js
@@ -2,70 +2,12 @@ import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {screens} from './constants/screens';
-import I18n from 'react-native-i18n';
-import {toLowerCaseFirstLetter} from './utils/string';
-import {setLanguage} from './utils/asyncStorage';
-import {LanguageContext} from './providers/LanguageProvider';
-import ImageButton from './components/ImageButton';
 
 const screensObject = Object.assign(
   {},
   ...screens.map(screen => ({[screen.name]: screen.component})),
 );
 
-const renderLanguageButton = () => {
-  return (
-    <LanguageContext.Consumer>
-      {context =>
-        context.language === 'pl' ? (
-          <ImageButton
-            onPress={() => {
-              setLanguage('en').then(() => {
-                context.showLoader();
-                context.setLanguage('en');
-              });
-            }}
-            buttonStyle={{
-              padding: 15,
-              alignItems: 'center',
-            }}
-            code={'PL'}
-          />
-        ) : (
-          <ImageButton
-            onPress={() =>
-              setLanguage('pl').then(() => {
-                context.showLoader();
-                context.setLanguage('pl');
-              })
-            }
-            buttonStyle={{
-              padding: 15,
-              alignItems: 'center',
-            }}
-            code={'GB'}
-          />
-        )
-      }
-    </LanguageContext.Consumer>
-  );
-};
-
-const renderName = routeName => {
-  return I18n.t(`routeTitle.${toLowerCaseFirstLetter(routeName)}`);
-};
-const Screens = createBottomTabNavigator(screensObject, {
-  navigationOptions: ({navigation}) => ({
-    title: renderName(navigation.state.routeName),
-    headerStyle: {
-      backgroundColor: '#f4511e',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
-    headerRight: renderLanguageButton(),
-  }),
-});
+const Screens = createBottomTabNavigator(screensObject);
 
 export default createAppContainer(Screens);
